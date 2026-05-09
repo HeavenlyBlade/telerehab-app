afa6a48aba91077aad4291f613e1c1d1b899db26
Benha Mine <119611900+HeavenlyBlade@users.noreply.github.com>
2026-05-10 06:36:08 +0800

    Fix CORS for Vercel URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://*.vercel.app",  # allows all vercel deployments
+        "https://telerehab-app.vercel.app",  # allows all vercel deployments
     ],
     allow_credentials=True,
     allow_methods=["*"],
